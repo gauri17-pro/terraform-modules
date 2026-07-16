b97d736b43e69c75873e2719f373d075f1be18e9
shirkang <gauri.shirkande@roche.com>
2026-07-16 22:40:14 +0530

    Adding releaser for modules

---
+++ aws/ec2/main.tf
@@ -0,0 +1,51 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+}
+
+resource "aws_instance" "ec2_instance" {
+    ami                    = data.aws_ami.ubuntu.id
+    instance_type          = var.instance_type
+    vpc_security_group_ids = [aws_security_group.sg.id]
+    key_name               = var.key_name
+
+    tags = {
+        Name = "terraform-instance"
+    }
+}
+
+resource "aws_security_group" "sg" {
+    name = "sg-1"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners      = ["099720109477"]
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+}
---
+++ aws/ec2/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "key_name" {
+    description = "EC2 Key Pair name"
+    type        = string
+}
